e7548df3bcbad65d00cfba2ba3f47efe47cf2309
albummi <110391540+albummi@users.noreply.github.com>
2025-01-26 20:25:11 +0100

    Update config_flow.py

--- custom_components/timed_entity_card/config_flow.py
+++ custom_components/timed_entity_card/config_flow.py
@@ -1,10 +1,9 @@
 from homeassistant import config_entries
 from homeassistant.core import callback
-from homeassistant.helpers import config_validation as cv
 import voluptuous as vol
+from homeassistant.helpers import config_validation as cv
 
-from .const import DOMAIN  # Sicherstellen, dass DOMAIN korrekt importiert wird
-
+from .const import DOMAIN
 
 class TimedEntityCardConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Timed Entity Card."""
@@ -16,26 +15,18 @@ class TimedEntityCardConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         errors = {}
 
         if user_input is not None:
-            # Validiere die Benutzereingabe
-            if not user_input.get("main_entity"):
-                errors["base"] = "entity_required"
-            else:
-                # Erstelle den Eintrag, wenn alle Eingaben korrekt sind
-                return self.async_create_entry(title="Timed Entity Card", data=user_input)
+            # Wenn die Eingabe korrekt ist, wird die Konfiguration erstellt
+            return self.async_create_entry(title="Timed Entity Card", data=user_input)
 
-        # Zeige das Formular erneut, wenn es Fehler gibt
-        return self.async_show_form(
-            step_id="user",
-            data_schema=self._get_schema(),
-            errors=errors,
-        )
-
-    @staticmethod
-    @callback
-    def _get_schema():
-        """Return the schema for the form."""
-        return vol.Schema({
+        # Schema für das Formular
+        data_schema = vol.Schema({
             vol.Required("main_entity"): cv.entity_id,
             vol.Optional("default_time", default="00:05:00"): str,
             vol.Optional("additional_entities", default=[]): cv.multi_select({})
         })
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=data_schema,
+            errors=errors,
+        )
