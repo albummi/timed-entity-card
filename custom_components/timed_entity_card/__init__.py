f794ed03e866df0960bede76c04d3265b8792aee
albummi <110391540+albummi@users.noreply.github.com>
2025-01-26 21:09:36 +0100

    f

--- custom_components/timed_entity_card/__init__.py
+++ custom_components/timed_entity_card/__init__.py
@@ -1,10 +1,15 @@
-from homeassistant import config_entries
-from homeassistant.const import CONF_NAME
-from .const import DOMAIN
+import logging
 
-async def async_setup(hass, config):
+from homeassistant.core import HomeAssistant
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the Timed Entity Card integration."""
+    _LOGGER.debug("Setting up Timed Entity Card integration...")
     return True
 
-async def async_setup_entry(hass, entry):
-    """Set up the timed entity card integration."""
+async def async_setup_entry(hass: HomeAssistant, entry):
+    """Set up the integration from a config entry."""
+    _LOGGER.debug("Setting up Timed Entity Card from config entry...")
     return True
--- custom_components/timed_entity_card/config_flow.py
+++ custom_components/timed_entity_card/config_flow.py
@@ -1,7 +1,8 @@
 import logging
-from homeassistant import config_entries
-from homeassistant.helpers import config_validation as cv
 import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.core import callback
+from homeassistant.helpers import config_validation as cv
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
@@ -13,9 +14,11 @@ class TimedEntityCardConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
+        _LOGGER.debug("User input received: %s", user_input)  # Debugging line for inputs
         errors = {}
 
         if user_input is not None:
+            # We handle input here by creating a new entry
             return self.async_create_entry(title="Timed Entity Card", data=user_input)
 
         data_schema = vol.Schema({
